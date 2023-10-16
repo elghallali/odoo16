4ab760f4a59a1a23448a715eb52d808a84e5bd59
Yassine EL GHALLALI <35077623+elghallali@users.noreply.github.com>
2023-10-16 23:13:32 +0100

    Initial commit

---
+++ dev/dsef_hospital/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Hospital Management System',
+    'author' : 'Yassine EL GHALLALI',
+    'website':'www.github.com/elghallali',
+    'summary': 'Odoo 16 Development',
+    'depends':['mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient.xml',
+    ],
+}
